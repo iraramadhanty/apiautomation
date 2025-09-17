6918e94d04e3eb7fd1ac0aac206e4e1cb3756611
@irarmdhnty11 <irarmdhnty11@gmail.com>
2025-09-17 09:59:43 +0700

    add priority

--- src/test/java/ApiImplementation.java
+++ src/test/java/ApiImplementation.java
@@ -71,7 +71,7 @@ public class ApiImplementation {
         Assert.assertTrue(response.jsonPath().getString("booking.lastname").equals("Brown"), "Last name should be Brown" );
     }
 
-    @Test (dependsOnMethods = {"create_booking"}, priority = 2)
+    @Test (priority = 2)
     public void update_booking(){
         String requestBody = "{\r\n" + //
                         "    \"firstname\" : \"James\",\r\n" + //
@@ -118,7 +118,7 @@ public class ApiImplementation {
         Assert.assertTrue(response.jsonPath().getString("firstname").equals("James"), "update first name to James");
     }
 
-    @Test (dependsOnMethods = {"create_booking"}, priority = 3)
+    @Test (priority = 3)
     public void delete_booking(){
         Response response = given()
             .baseUri("https://restful-booker.herokuapp.com/booking/"+idBooking)
